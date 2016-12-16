6fb5008ff295792dce27c3ec65d6103dd5b299e8
Patrick Chan <patrickchan1999@gmail.com>
2016-12-16 12:32:21 -0500

    Fixed typeError

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@ var list = document.getElementById("thelist");
 var addChild = function() {
     var tag = document.createElement("li");
     tag.innerHtml = "New Thing";
-    list.append(tag)
+    list.appendChild(tag)
 }
 
 var b = document.getElementById("b");
