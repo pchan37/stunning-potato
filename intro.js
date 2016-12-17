9009884dc6d378146920253eadf1410a336f2b0b
fhaque1 <farhan3231@gmail.com>
2016-12-17 14:25:46 -0500

    started work on remove

--- intro.js
+++ intro.js
@@ -3,7 +3,16 @@ var addChild = function() {
     var tag = document.createElement("li");
     tag.innerHTML = "New Thing";
     list.appendChild(tag)
-}
+};
 
 var b = document.getElementById("b");
 b.addEventListener("click", addChild);
+
+
+var listElements = document.getElementsByTagName("li");
+var removeChild = function(i) {
+	listElements[i].remove();
+};
+
+for (i = 0; i < listElements.length; i++)
+	listElements[i].addEventListener("click", removeChild(i));
