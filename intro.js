758f7152b910aed0af5ba2ed1337384f0e980d73
fhaque1 <farhan3231@gmail.com>
2016-12-17 15:10:48 -0500

    added Hover Feature

--- intro.js
+++ intro.js
@@ -2,7 +2,7 @@ var list = document.getElementById("thelist");
 var addChild = function() {
     var tag = document.createElement("li");
     tag.innerHTML = "New Thing";
-	tag.addEventListener("click", removeChild)
+	eventListener(tag);
     list.appendChild(tag)
 };
 
@@ -15,5 +15,25 @@ var removeChild = function() {
 	this.remove();
 };
 
-for (i = 0; i < listElements.length; i++)
-	listElements[i].addEventListener("click", removeChild);
+var x = document.getElementById("h").innerHTML;
+
+var changeHeader = function() {
+	var h = document.getElementById("h");
+	h.innerHTML = this.innerHTML;
+};
+
+var changeHeaderBack = function() {
+	var h = document.getElementById("h");
+	h.innerHTML = "Hello World!";
+};
+
+var eventListener = function(elem) {
+	elem.addEventListener("click", removeChild);
+	elem.addEventListener("mouseover",changeHeader);
+	elem.addEventListener("mouseout",changeHeaderBack);
+};
+
+for (i = 0; i < listElements.length; i++){
+	eventListener(listElements[i]);
+};
+
